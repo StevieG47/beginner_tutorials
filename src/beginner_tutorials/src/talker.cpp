19a0b81f510034d61e8d52cbc3abc1a5d0b9bcc8
Steven Gambino <47stevo@gmail.com>
2017-03-28 17:47:52 -0500

    Modified publisher node for custom string message.

--- src/beginner_tutorials/src/talker.cpp
+++ src/beginner_tutorials/src/talker.cpp
@@ -60,7 +60,8 @@ int main(int argc, char **argv) {
     std_msgs::String msg;
 
     std::stringstream ss;
-    ss << "hello world " << count;
+    ss << "Steven Gambino, ENPM808X";
+//<< count;
     msg.data = ss.str();
 
     ROS_INFO("%s", msg.data.c_str());
