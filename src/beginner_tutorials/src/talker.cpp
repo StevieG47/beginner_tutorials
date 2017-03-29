34ec3c2c311fdb285650ef0598951eea8d6aae4d
Steven Gambino <47stevo@gmail.com>
2017-03-28 20:38:30 -0500

    Modified build/run instructions on README

--- src/beginner_tutorials/src/talker.cpp
+++ src/beginner_tutorials/src/talker.cpp
@@ -1,7 +1,27 @@
+/* MIT License
+ Copyright (c) 2017 Steven Gambino
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+#include <sstream>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
-#include <sstream>
+
 
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
@@ -60,8 +80,7 @@ int main(int argc, char **argv) {
     std_msgs::String msg;
 
     std::stringstream ss;
-    ss << "Steven Gambino, ENPM808X";
-//<< count;
+    ss << "Steven Gambino, ENPM808X " << count;
     msg.data = ss.str();
 
     ROS_INFO("%s", msg.data.c_str());
