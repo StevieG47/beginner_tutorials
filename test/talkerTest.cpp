dc4f657250291be31acbe48dab00ead5fe2cfe62
Steven Gambino <47stevo@gmail.com>
2017-04-10 09:05:07 -0500

    Fixed some typos for gtest

--- test/talkerTest.cpp
+++ test/talkerTest.cpp
@@ -1,20 +1,23 @@
 #include <ros/ros.h>
 #include <ros/service_client.h>
 #include <gtest/gtest.h>
+#include "beginner_tutorials/NewMessage.h"
+
 
 TEST(TestSuite, serviceTest)
 {
   ros::NodeHandle n;
   
-  ros::ServiceClient client = n.serviceClient<beginner_tuorials::NewMessage>(
-      "update_service);
+  ros::ServiceClient client = n.serviceClient < beginner_tutorials::NewMessage
+      > (
+      "update_service");
   bool exists(client.waitForExistence(ros::Duration(1)));
   EXPECT_TRUE(exists);
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc,argv,'testnode');
+  ros::init(argc, argv, "talkerTest");
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
